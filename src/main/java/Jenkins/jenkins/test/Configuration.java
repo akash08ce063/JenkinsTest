86db2dc1cae519e5037d6d0b5b33364f18606b74
Akash Patel <apatel2@wm100832.local>
2016-06-27 17:14:16 -0400

    fifth time commit

--- src/main/java/Jenkins/jenkins/test/Configuration.java
+++ src/main/java/Jenkins/jenkins/test/Configuration.java
@@ -4,5 +4,4 @@ public class Configuration {
 	public static String mySqlConnector = "mysql";
 	public static String mysqlUsername = "root";
 	public static String mysqlPassword = "root";
-	public static String applicationAuthString = "34232@@asfasARTT";
 }
